1d5caf37cf8a0c264669faf622ebcb2edecbced7
Nicolas Polizzo <nicolas.polizzo@theprogrammaticcompany.com>
2016-08-12 17:11:02 +0200

    bootstrap redux application

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+var debug       = process.env.NODE_ENV !== 'production',
+  webpack     = require('webpack'),
+  path        = require('path'),
+  HWP         = require('html-webpack-plugin'),
+  HWPConfig   = new HWP({ template : __dirname + '/app/index.html', filename : 'index.html', inject : 'body' });
+
+module.exports = {
+  context : path.join(__dirname, 'app'),
+  devtool : debug ? 'inline-sourcemap' : null,
+  entry   : './index.js',
+  module  : {
+    loaders : [
+      {
+        test    : /\.jsx?$/,
+        exclude : /(node_modules|bower_components)/,
+        loader  : 'babel-loader',
+        query   : {
+          presets : ['react', 'es2015', 'stage-0'],
+          plugins : ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy']
+        }
+      },
+      {
+        test    : /\.scss$/,
+        loader  : 'style!css!sass'
+      }
+    ]
+  },
+  output  : {
+    path      : path.resolve(__dirname, 'dist'),
+    filename  : 'client.min.js'
+  },
+  plugins : debug ? [
+    HWPConfig
+  ] : [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({ mangle : false, sourcemap : false }),
+    HWPConfig
+  ]
+};
