076ab1d0bf6f9bf61dc144a4608e9fa4b6241706
Freezystem <freezystem@gmail.com>
2016-09-04 15:49:56 +0200

    arrange component tests to be in the right order

--- test/todos/components.specs.js
+++ test/todos/components.specs.js
@@ -11,6 +11,74 @@ import {
 }                   from '../../app/modules/todos/TodoApp.component';
 
 describe('components', () => {
+  describe('<Todo />', () => {
+    let attrs = {},
+      wrapper = null;
+
+    beforeEach(() => {
+      attrs = {
+        completed      : true,
+        label          : 'todo',
+        onClick        : expect.createSpy(),
+        onButtonClick  : expect.createSpy()
+      };
+
+      wrapper = mount(<Todo {...attrs} />);
+    });
+
+    it('should render a todo', () => {
+      expect(wrapper.find('.todoList_itemLabel').text()).toBe('todo');
+      expect(wrapper.find('.todoList_itemButton').text()).toExist();
+    });
+
+    it('should call onClick() when label is clicked', () => {
+      wrapper.find('.todoList_itemLabel').simulate('click');
+
+      expect(attrs.onClick.calls.length).toBe(1);
+    });
+
+    it('should call onButtonClick() when button is clicked', () => {
+      wrapper.find('.todoList_itemButton').simulate('click');
+
+      expect(attrs.onButtonClick.calls.length).toBe(1);
+    });
+  });
+
+  describe('<TodoList />', () => {
+    let attrs = {},
+      wrapper = null;
+
+    beforeEach(() => {
+      attrs = {
+        filteredTodos       : [
+          {label: 'todo1', id: 111, complete: false},
+          {label: 'todo2', id: 222, complete: false},
+          {label: 'todo3', id: 333, complete: false},
+        ],
+        onTodoClick         : expect.createSpy(),
+        onTodoButtonClick   : expect.createSpy()
+      };
+
+      wrapper = mount(<TodoList {...attrs} />);
+    });
+
+    it('should render filtered todos', () => {
+      const firstTodo = wrapper.find(Todo).at(0);
+      const secondTodo = wrapper.find(Todo).at(1);
+      const thirdTodo = wrapper.find(Todo).at(2);
+
+      expect(wrapper.find('.todoList').children().length).toBe(3);
+
+      expect(firstTodo.key()).toBe('111');
+      expect(secondTodo.key()).toBe('222');
+      expect(thirdTodo.key()).toBe('333');
+
+      expect(firstTodo.find('.todoList_itemLabel').text()).toBe('todo1');
+      expect(secondTodo.find('.todoList_itemLabel').text()).toBe('todo2');
+      expect(thirdTodo.find('.todoList_itemLabel').text()).toBe('todo3');
+    });
+  });
+
   describe('<TodoForm />', () => {
     let attrs = {},
       wrapper = null;
@@ -56,74 +124,6 @@ describe('components', () => {
       form.simulate('submit');
 
       expect(attrs.onTodoFormSubmit).toHaveBeenCalledWith('write test');
-    });
-  });
-
-  describe('<TodoList />', () => {
-    let attrs = {},
-      wrapper = null;
-
-    beforeEach(() => {
-      attrs = {
-        filteredTodos       : [
-          {label: 'todo1', id: 111, complete: false},
-          {label: 'todo2', id: 222, complete: false},
-          {label: 'todo3', id: 333, complete: false},
-        ],
-        onTodoClick         : expect.createSpy(),
-        onTodoButtonClick   : expect.createSpy()
-      };
-
-      wrapper = mount(<TodoList {...attrs} />);
-    });
-
-    it('should render filtered todos', () => {
-      const firstTodo = wrapper.find(Todo).at(0);
-      const secondTodo = wrapper.find(Todo).at(1);
-      const thirdTodo = wrapper.find(Todo).at(2);
-
-      expect(wrapper.find('.todoList').children().length).toBe(3);
-
-      expect(firstTodo.key()).toBe('111');
-      expect(secondTodo.key()).toBe('222');
-      expect(thirdTodo.key()).toBe('333');
-
-      expect(firstTodo.find('.todoList_itemLabel').text()).toBe('todo1');
-      expect(secondTodo.find('.todoList_itemLabel').text()).toBe('todo2');
-      expect(thirdTodo.find('.todoList_itemLabel').text()).toBe('todo3');
-    });
-  });
-
-  describe('<Todo />', () => {
-    let attrs = {},
-      wrapper = null;
-
-    beforeEach(() => {
-      attrs = {
-        completed      : true,
-        label          : 'todo',
-        onClick        : expect.createSpy(),
-        onButtonClick  : expect.createSpy()
-      };
-
-      wrapper = mount(<Todo {...attrs} />);
-    });
-
-    it('should render a todo', () => {
-      expect(wrapper.find('.todoList_itemLabel').text()).toBe('todo');
-      expect(wrapper.find('.todoList_itemButton').text()).toExist();
-    });
-
-    it('should call onClick() when label is clicked', () => {
-      wrapper.find('.todoList_itemLabel').simulate('click');
-
-      expect(attrs.onClick.calls.length).toBe(1);
-    });
-
-    it('should call onButtonClick() when button is clicked', () => {
-      wrapper.find('.todoList_itemButton').simulate('click');
-
-      expect(attrs.onButtonClick.calls.length).toBe(1);
     });
   });
 });
